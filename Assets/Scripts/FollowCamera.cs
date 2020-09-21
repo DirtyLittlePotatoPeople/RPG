adac43555712c3f8d74e4a765c9d6b7e82bab786
dlpp <DirtyLittlePotatoPeople@gmail.com>
2020-09-21 21:20:32 +0000

    Section 2
    Basic movement and camera functions implemented.

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+	// Configuration parameters
+	[SerializeField] Transform target;
+
+	// Cached references
+
+	// State
+
+
+	void Start()
+	{
+		
+	}
+
+	void LateUpdate()
+	{
+		transform.position = target.position;
+	}
+}
